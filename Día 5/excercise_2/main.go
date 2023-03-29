caf81e5ad62bb80045c8ea10552c93bc6ab70e52
Esteban Jiménez <esteban.jimenez@mercadolibre.com.co>
2023-03-29 10:17:16 -0500

    Subiendo ejercicios 2 y 3 del día 5

---
+++ Día 5/excercise_2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+/* Ejercicio 2 - Impuestos de salario #2
+En tu función “main”, definí una variable llamada “salary” y asignale un valor de tipo “int”.
+Creá un error personalizado con un struct que implemente “Error()” con el mensaje “Error: el salario es menor a 10.000" y lanzalo en caso de que “salary” sea menor o igual a  10.000. La validación debe ser hecha con la función “Is()” dentro del “main”.
+*/
+
+// Definimos una estructura llamada salaryError que tendrá un campo message de tipo string
+type salaryError struct {
+	message string
+}
+
+// Implementamos el método Error() para la estructura salaryError
+func (salary *salaryError) Error() string {
+	return salary.message
+}
+
+func main() {
+	var salary int = 9000 // Creamos una variable int salary con valor 9000
+
+	// Comprobamos si el valor de salary es menor o igual a 10000
+	if salary <= 10000 {
+		// Creamos un nuevo error personalizado de tipo salaryError con el mensaje indicado
+		err := &salaryError{"Error: el salario es menor a 10.000"}
+
+		// Usamos la función Is() de la librería errors para comprobar si el error es de tipo salaryError
+		if errors.Is(err, &salaryError{}) {
+			fmt.Println(err) // Imprimimos el mensaje de error en la consola
+		}
+	}
+}
---
+++ Día 5/excercise_3/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+/*
+Ejercicio 3 - Impuestos de salario #3
+
+Hacé lo mismo que en el ejercicio anterior pero reformulando el código para que, en reemplazo de “Error()”,  se implemente “errors.New()”.
+*/
+func main() {
+	var salary int = 9000 // Creamos una variable salary con valor 9000
+	// Comprobamos si el valor de salary es menor o igual a 10000
+	if salary <= 10000 {
+		/* Creamos un nuevo error personalizado utilizando la función errors.New() */
+		err := errors.New("error: el salario es menor a 10.000")
+		// Usamos la función Is() de la librería errors para comprobar si el error es de tipo error
+		if errors.Is(err, errors.New("")) {
+			fmt.Println(err) // Imprimimos el mensaje de error en la consola
+		}
+	}
+
+}
